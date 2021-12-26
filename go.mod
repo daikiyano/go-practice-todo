8fdc95cc6de89e5d297af6e161a877d33e74c798
矢野大樹 <daikitech0123@gmail.com>
2021-12-26 18:46:23 +0900

    add config

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-todo-practice
+
+go 1.14
+
+require gopkg.in/ini.v1 v1.66.2
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"go-todo-practice/config"
+	"log"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+	log.Println("test")
+}
