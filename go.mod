c4d6cf997939fb4dce512dcc2723ae2bb062a32d
矢野大樹 <daikitech0123@gmail.com>
2021-12-27 22:49:28 +0900

    CreateUser

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-todo-practice
 go 1.14
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.9
 	gopkg.in/ini.v1 v1.66.2
 )
--- main.go
+++ main.go
@@ -12,4 +12,11 @@ func main() {
 	//fmt.Println(config.Config.LogFile)
 	//log.Println("test")
 	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@gmail.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+	u.CreateUser()
 }
