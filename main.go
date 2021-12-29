e761b3a70bdcfcd7f9450c79931b46cc36266300
矢野大樹 <daikitech0123@gmail.com>
2021-12-29 10:18:42 +0900

    add UserUpdate

--- main.go
+++ main.go
@@ -13,10 +13,17 @@ func main() {
 	//log.Println("test")
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@gmail.com"
-	u.PassWord = "testtest"
+	//u := &models.User{}
+	//u.Name = "test"
+	//u.Email = "test@gmail.com"
+	//u.PassWord = "testtest"
+	//fmt.Println(u)
+	//u.CreateUser()
+	u, _ := models.GetUser(1)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
 	fmt.Println(u)
-	u.CreateUser()
 }
