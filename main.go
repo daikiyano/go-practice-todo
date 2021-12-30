457a12f56d5c3e8d36f6eba08973458b9fb5af60
矢野大樹 <daikitech0123@gmail.com>
2021-12-30 11:03:03 +0900

    add Gettodos

--- main.go
+++ main.go
@@ -11,20 +11,24 @@ func main() {
 	//fmt.Println(config.Config.DbName)
 	//fmt.Println(config.Config.LogFile)
 	//log.Println("test")
-	fmt.Println(models.Db)
-
+	//fmt.Println(models.Db)
+	//
 	//u := &models.User{}
 	//u.Name = "test"
 	//u.Email = "test@gmail.com"
 	//u.PassWord = "testtest"
 	//fmt.Println(u)
 	//u.CreateUser()
-	u, _ := models.GetUser(1)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	//u, _ := models.GetUser(1)
+	//
+	//u.Name = "Test2"
+	//u.Email = "test2@example.com"
+	//u.UpdateUser()
+	//u.DeleteUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
+	//user, _ := models.GetUser(2)
+	//user.CreateTodo("First Todo")
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
