c6baf04a8883bc3f2e1b1bf33245fe9f3487d21f
矢野大樹 <daikitech0123@gmail.com>
2022-01-02 16:52:48 +0900

    add sessions table

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "go-todo-practice/app/controllers"
+import (
+	"fmt"
+	"go-todo-practice/app/controllers"
+	"go-todo-practice/app/models"
+)
 
 func main() {
-
+	fmt.Println(models.Db)
 	controllers.StartMainServer()
 }
