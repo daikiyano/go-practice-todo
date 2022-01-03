14ba75106d8f9c4ba5bb062ddd49330fb6fc91e4
矢野大樹 <daikitech0123@gmail.com>
2022-01-03 10:34:40 +0900

    Check Session method

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"go-todo-practice/app/controllers"
 	"go-todo-practice/app/models"
+	"log"
 )
 
 func main() {
 	fmt.Println(models.Db)
-	//controllers.StartMainServer()
+	controllers.StartMainServer()
 	user, _ := models.GetUserByEmail("test@example.com")
 	fmt.Println(user)
+
+	session, err := user.CreateSession()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(session)
+
+	valid, _ := session.CheckSession()
+	fmt.Println(valid)
 }
